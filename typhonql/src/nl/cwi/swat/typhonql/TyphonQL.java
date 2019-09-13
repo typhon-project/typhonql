0282591d157c8aa21cec40a960589fc2529d7439
Pablo Inostroza Valdera <pinoval@gmail.com>
2019-09-13 17:22:24 +0200

    Added method for booting the connections

--- typhonql/src/nl/cwi/swat/typhonql/TyphonQL.java
+++ typhonql/src/nl/cwi/swat/typhonql/TyphonQL.java
@@ -2,6 +2,10 @@ package nl.cwi.swat.typhonql;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.client.ClientProtocolException;
@@ -10,11 +14,12 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.bson.BsonArray;
-import org.eclipse.emf.common.util.URI;
+import org.bson.BsonDocument;
+import org.bson.BsonInvalidOperationException;
+import org.bson.BsonValue;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
-import org.eclipse.xtext.resource.XtextResourceSet;
 import org.rascalmpl.interpreter.TypeReifier;
 import org.rascalmpl.interpreter.utils.RuntimeExceptionFactory;
 
@@ -45,7 +50,7 @@ public class TyphonQL {
 	// TODO: we might have to delay returning the schema, since the platform
 	// might not be ready when this code is run.
 	public IConstructor bootTyphonQL(IValue typeOfTyphonML, ISourceLocation path) {
-		Connections.boot();
+		//Connections.boot();
 
 		TypeStore ts = new TypeStore(); // start afresh
 		
@@ -64,7 +69,47 @@ public class TyphonQL {
 		}
 	}
 	
-	public IString readHttpLocation(ISourceLocation path) {
+	public void bootConnections(ISourceLocation path) {
+		// TODO eliminate this code as soon as we have extended REST API
+		Map<String, DBType> dbTypes = new HashMap<>();
+		dbTypes.put("MongoDb", DBType.documentdb);
+		dbTypes.put("MariaDB", DBType.relationaldb);
+		// end todo
+		
+		String json = readHttp(path);
+		BsonArray array = BsonArray.parse(json);
+		List<ConnectionInfo> infos = new ArrayList<ConnectionInfo>();
+		for (BsonValue v : array.getValues()) {
+			BsonDocument d = v.asDocument();
+			try {
+				ConnectionInfo info = new ConnectionInfo(
+					d.getString("host").getValue(), 
+					d.getNumber("port").intValue(), 
+					d.getString("name").getValue(), 
+					dbTypes.get(d.getString("dbType").getValue()),
+					d.getString("dbType").getValue(),
+					d.getString("username").getValue(),
+					d.getString("password").getValue());
+				infos.add(info);
+			} catch (BsonInvalidOperationException e) {
+				// TODO not do anything if row of connection information is unparsable
+			}
+		}
+		// TODO remove this workaround because the MariaDB info in the endpoint is incorrect
+		// and let Rascal code parse the json
+		infos.add(new ConnectionInfo("localhost", 3306, "RelationalDatabase", DBType.relationaldb, 
+				"MariaDB", "root", "example"));
+		Connections.boot(infos.toArray(new ConnectionInfo[0]));
+	}
+
+	public IString readHttpModel(ISourceLocation path) {
+		String json = readHttp(path);
+		BsonArray array = BsonArray.parse(json);
+		String contents = array.get(0).asDocument().getString("contents").getValue();
+		return vf.string(contents);
+	}
+
+	private String readHttp(ISourceLocation path) {
 		CloseableHttpClient httpclient = HttpClients.createDefault();
 		HttpGet httpGet = new HttpGet(path.getURI());
 		CloseableHttpResponse response1;
@@ -78,19 +123,17 @@ public class TyphonQL {
 			throw RuntimeExceptionFactory.io(vf.string("Problem executing HTTP request"), null, null);
 		}
 		try {
-		    HttpEntity entity1 = response1.getEntity();
-		    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		    entity1.writeTo(baos);
-		    String json = new String(baos.toByteArray());
-		    BsonArray array = BsonArray.parse(json);
-			String contents = array.get(0).asDocument().getString("contents").getValue();
-			return vf.string(contents);
-			
+			HttpEntity entity1 = response1.getEntity();
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			entity1.writeTo(baos);
+			String s = new String(baos.toByteArray());
+			return s;
+
 		} catch (IOException e) {
 			e.printStackTrace();
 			throw RuntimeExceptionFactory.io(vf.string("Problem reading from HTTP resource"), null, null);
 		} finally {
-		    try {
+			try {
 				response1.close();
 			} catch (IOException e) {
 				e.printStackTrace();
