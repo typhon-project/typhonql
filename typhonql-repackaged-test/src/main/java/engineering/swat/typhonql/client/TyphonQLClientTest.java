ddbda465ae5a2c39dbfde64d763f6e7f6f3edb42
Davy Landman <davy.landman@swat.engineering>
2019-10-16 13:08:41 +0200

    Added a new headless jar builder

--- typhonql-repackaged-test/src/main/java/engineering/swat/typhonql/client/TyphonQLClientTest.java
+++ typhonql-repackaged-test/src/main/java/engineering/swat/typhonql/client/TyphonQLClientTest.java
@@ -65,7 +65,7 @@ public class TyphonQLClientTest {
 				new DatabaseInfo("localhost", 3306, "RelationalDatabase", DBType.relationaldb, new MariaDB().getName(),
 						"root", "example") };
 	
-		PolystoreConnection conn = new PolystoreConnection(schema, Arrays.asList(infos), false);
+		PolystoreConnection conn = new PolystoreConnection(schema, Arrays.asList(infos), true);
 		IValue iv = conn.executeQuery("from User u select u");
 		System.out.println(iv);
 	
