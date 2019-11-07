ea7f739a6b9b86e3c50071ffbf3e12c76f1ae691
Pablo Inostroza Valdera <pinoval@gmail.com>
2019-11-07 17:08:26 +0100

    Converting Rascal working set into simpler structure

--- typhonql/src/nl/cwi/swat/typhonql/workingset/Entity.java
+++ typhonql/src/nl/cwi/swat/typhonql/workingset/Entity.java
@@ -1,7 +1,16 @@
 package nl.cwi.swat.typhonql.workingset;
 
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.stream.Collectors;
+
+import io.usethesource.vallang.IConstructor;
+import io.usethesource.vallang.IList;
+import io.usethesource.vallang.IMap;
+import io.usethesource.vallang.IString;
+import io.usethesource.vallang.IValue;
 
 public class Entity {
 	public String name;
@@ -33,6 +42,26 @@ public class Entity {
 				+ String.join(", ", fields.entrySet().stream().map(e -> e.getKey() + ":" + e.getValue())
 						.collect(Collectors.toList()))
 				+ "] }";
+	}
+
+	public static Entity fromIValue(IValue v) {
+		if (v instanceof IConstructor) {
+			IConstructor con = (IConstructor) v;
+			IString name = (IString) con.get("name");
+			IString uuid = (IString) con.get("uuid");
+			Map<String, Object> fields = new HashMap<String, Object>();
+			IMap map = (IMap) con.get("fields");
+			Iterator<Entry<IValue, IValue>> iter = map.entryIterator();
+			while (iter.hasNext()) {
+				Entry<IValue, IValue> entry = iter.next();
+				IString key = (IString) entry.getKey();
+				IValue object =  entry.getValue();
+				fields.put(key.getValue(), object);
+			}
+			return new Entity(name.getValue(), uuid.getValue(), fields);
+		}
+		else
+			throw new RuntimeException("IValue does not represent an entity");
 	}
 	
 	
--- typhonql/src/nl/cwi/swat/typhonql/workingset/WorkingSet.java
+++ typhonql/src/nl/cwi/swat/typhonql/workingset/WorkingSet.java
@@ -1,17 +1,42 @@
 package nl.cwi.swat.typhonql.workingset;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
+import io.usethesource.vallang.IList;
+import io.usethesource.vallang.IMap;
+import io.usethesource.vallang.IString;
 import io.usethesource.vallang.IValue;
 
 @SuppressWarnings("serial")
 public class WorkingSet extends HashMap<String, List<Entity>>{
 	
 	public static WorkingSet fromIValue(IValue v) {
-		WorkingSet ws = new WorkingSet();
-		
-		return ws;
+		//map[str entity, list[Entity] entities];
+		if (v instanceof IMap) {
+			IMap map = (IMap) v;
+			WorkingSet ws = new WorkingSet();
+			Iterator<Entry<IValue, IValue>> iter = map.entryIterator();
+			while (iter.hasNext()) {
+				Entry<IValue, IValue> entry = iter.next();
+				IString key = (IString) entry.getKey();
+				IList entries = (IList) entry.getValue();
+				Iterator<IValue> entryIter = entries.iterator();
+				List<Entity> entities = new ArrayList<Entity>();
+				while (entryIter.hasNext()) {
+					IValue current = entryIter.next();
+					Entity e = Entity.fromIValue(entry.getValue());
+					entities.add(e);
+				}
+				
+				ws.put(key.getValue(), entities);
+			}
+			return ws;
+		}
+		else
+			throw new RuntimeException("IValue does not represent a working set");
 		
 	}
 }
