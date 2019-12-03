0cfd576f58f2ee94ba1ba7a12fb99a95ef3941ed
Pablo Inostroza Valdera <pinoval@gmail.com>
2019-12-03 12:52:02 +0100

    Added entity ref as possible field value

--- typhonql/src/nl/cwi/swat/typhonql/workingset/Entity.java
+++ typhonql/src/nl/cwi/swat/typhonql/workingset/Entity.java
@@ -1,17 +1,19 @@
 package nl.cwi.swat.typhonql.workingset;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonUnwrapped;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 import io.usethesource.vallang.IConstructor;
 import io.usethesource.vallang.IInteger;
+import io.usethesource.vallang.IList;
 import io.usethesource.vallang.IMap;
 import io.usethesource.vallang.IString;
 import io.usethesource.vallang.ITuple;
@@ -22,7 +24,8 @@ public class Entity {
 	public String name;
 	public String uuid;
 	
-	@JsonSerialize
+	//@JsonDeserialize(contentUsing = FieldValueDeserializer.class)
+	@JsonDeserialize(contentConverter = EntityRefConverter.class)
 	public Map<String, Object> fields;
 	
 	public Entity(String name, String uuid) {
@@ -34,6 +37,10 @@ public class Entity {
 		this.name = name;
 		this.uuid = uuid;
 		this.fields = fields;
+	}
+	
+	public Entity() {
+		super();
 	}
 	
 	@JsonProperty("type")
@@ -91,8 +98,22 @@ public class Entity {
 		else if (object instanceof IString) {
 			return ((IString) object).getValue();
 		}
+		else if (object instanceof IList) {
+			Iterator<IValue> iter = ((IList) object).iterator();
+			List<Object> lst = new ArrayList<Object>();
+			while (iter.hasNext()) {
+				IValue v = iter.next();
+				lst.add(toJava(v));
+			}
+			return lst;
+		}
 		else if (object instanceof IConstructor) {
 			// TODO do the resolution for entities
+			IConstructor con = (IConstructor) object;
+			if (con.getName().equals("uuid")) {
+				IString uuid = (IString) con.getChildren().iterator().next();
+				return new EntityRef(uuid.getValue());
+			}
 			
 		}
 		
