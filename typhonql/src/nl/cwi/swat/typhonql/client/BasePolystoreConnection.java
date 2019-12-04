d4ac0d3915b2af09dc78c16334f90070419affd2
Davy Landman <davy.landman@swat.engineering>
2019-12-04 13:52:35 +0100

    Got the evaluator sensitive code working for stable again

--- typhonql/src/nl/cwi/swat/typhonql/client/BasePolystoreConnection.java
+++ typhonql/src/nl/cwi/swat/typhonql/client/BasePolystoreConnection.java
@@ -10,6 +10,7 @@ import org.rascalmpl.interpreter.Evaluator;
 import org.rascalmpl.interpreter.env.GlobalEnvironment;
 import org.rascalmpl.interpreter.env.ModuleEnvironment;
 import org.rascalmpl.interpreter.load.StandardLibraryContributor;
+import org.rascalmpl.interpreter.utils.RascalManifest;
 import org.rascalmpl.library.util.PathConfig;
 import org.rascalmpl.uri.URIResolverRegistry;
 import org.rascalmpl.uri.URIUtil;
@@ -27,6 +28,7 @@ import nl.cwi.swat.typhonql.Connections;
  */
 public abstract class BasePolystoreConnection extends PolystoreConnection {
 	
+	private static final String RASCAL_MF = "META-INF/RASCAL.MF";
 	protected static final String LOCALHOST = "localhost";
 	protected final ConcurrentSoftReferenceObjectPool<Evaluator> evaluators;
 	
@@ -38,6 +40,10 @@ public abstract class BasePolystoreConnection extends PolystoreConnection {
         return Math.max(1, numberOfEvaluators);
 	}
 
+	
+	private static boolean hasRascalMF(ISourceLocation root) {
+		return URIResolverRegistry.getInstance().exists(URIUtil.getChildLocation(root, RascalManifest.META_INF_RASCAL_MF));
+	}
 
 	public BasePolystoreConnection(List<DatabaseInfo> infos) throws IOException {
 		// bootstrap the connections
@@ -46,23 +52,45 @@ public abstract class BasePolystoreConnection extends PolystoreConnection {
 				// we prepare some configuration that every evaluator will need
 				
 
+				ISourceLocation root = URIUtil.correctLocation("project", "typhonql", null);
 				URIResolverRegistry reg = URIResolverRegistry.getInstance();
-				if (!reg.getRegisteredInputSchemes().contains("project") && !reg.getRegisteredLogicalSchemes().contains("project")) {
-					// project URI is not supported, so we have to add support for this (we have to do this only once)
-		            ISourceLocation projectRoot;
+				if (!hasRascalMF(root)) {
+					// project not available, switch to lib
+					root = URIUtil.correctLocation("lib", "typhonql", null);
+				}
+				if (!hasRascalMF(root)) {
+					// maybe lib scheme does not exist yet, so we switch to the plugin scheme
+					root = URIUtil.correctLocation("plugin", "typhonql", null);
+				}
+				if (!hasRascalMF(root)) {
+					// we are not inside eclipse/OSGI, so we are in the headless version, so we have to help the registry in finding 
 		            try {
-		                projectRoot = URIUtil.createFileLocation(SimplePolystoreConnection.class.getProtectionDomain().getCodeSource().getLocation().getPath());
-		                if (projectRoot.getPath().endsWith(".jar")) {
-		                    projectRoot = URIUtil.changePath(URIUtil.changeScheme(projectRoot, "jar+" + projectRoot.getScheme()), projectRoot.getPath() + "!/");
+		                root = URIUtil.createFileLocation(SimplePolystoreConnection.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+		                if (root.getPath().endsWith(".jar")) {
+		                    root = URIUtil.changePath(URIUtil.changeScheme(root, "jar+" + root.getScheme()), root.getPath() + "!/");
 		                }
 		            } catch (URISyntaxException e) {
 		                throw new RuntimeException("Cannot get to root of the typhonql project", e);
 		            }
-					//reg.registerLogical(new ProjectURIResolver(projectRoot, "typhonql"));
-					// from now on, |project://typhonql/| should work
+				}
+				if (!hasRascalMF(root)) {
+					if (root.getScheme().equals("file") && root.getPath().endsWith("/target/classes/")) {
+						// last resort, we are in a 1st level eclipse on the typhonql project itself, now RASCAL.MF file is harder to find
+						try {
+							root = URIUtil.changePath(root, root.getPath().replace("/target/classes/", ""));
+						} catch (URISyntaxException e) {
+							throw new RuntimeException("Cannot get to root of the typhonql project", e);
+						}
+					}
 				}
 
-				PathConfig pcfg = PathConfig.fromSourceProjectRascalManifest(URIUtil.correctLocation("lib", "typhonql", null));
+				if (!hasRascalMF(root)) {
+					System.err.println("Running polystore jar in a strange context, cannot find rascal.mf file & and related modules");
+					System.err.println("Last location tried: " + root);
+				}
+
+				
+				PathConfig pcfg = PathConfig.fromSourceProjectRascalManifest(root);
 				ClassLoader cl = new SourceLocationClassLoader(pcfg.getClassloaders(), SimplePolystoreConnection.class.getClassLoader());
 				
 				evaluators = new ConcurrentSoftReferenceObjectPool<>(10, TimeUnit.MINUTES, 1, calculateMaxEvaluators(), () -> {
