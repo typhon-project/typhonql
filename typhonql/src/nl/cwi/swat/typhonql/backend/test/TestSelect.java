78b9100ab1cea0a187f831fe9444a27af306300e
Pablo Inostroza Valdera <pinoval@gmail.com>
2019-12-12 22:31:35 +0100

    Updated tests

--- typhonql/src/nl/cwi/swat/typhonql/backend/test/TestSelect.java
+++ typhonql/src/nl/cwi/swat/typhonql/backend/test/TestSelect.java
@@ -9,7 +9,6 @@ import nl.cwi.swat.typhonql.backend.EntityModel;
 import nl.cwi.swat.typhonql.backend.MariaDBEngine;
 import nl.cwi.swat.typhonql.backend.MongoDBEngine;
 import nl.cwi.swat.typhonql.backend.ResultStore;
-import nl.cwi.swat.typhonql.backend.StringType;
 import nl.cwi.swat.typhonql.backend.TyphonType;
 import nl.cwi.swat.typhonql.workingset.Entity;
 import nl.cwi.swat.typhonql.workingset.WorkingSet;
@@ -120,16 +119,16 @@ public class TestSelect {
 		Engine e1 = new MariaDBEngine(store, "localhost", 3306, "Inventory", "root", "example");
 		Engine e2 = new MongoDBEngine(store, "localhost", 27018, "Reviews", "admin", "admin");
 		
-		e1.executeSelect("user", new String[] {}, "select * from User where `User.name` = \"Pablo\"");
+		e1.executeSelect("user", "select * from User where `User.name` = \"Pablo\"");
 		Map<String, Binding> map1 = new HashMap<String, Binding>();
 		map1.put("user_id", new Binding("user", "User"));
-		e2.executeSelect("review", new String[] {}, "Review\n{ user: \"${user_id}\" }", map1);
+		e2.executeSelect("review", "Review\n{ user: \"${user_id}\" }", map1);
 		
 		// Binding needs an extra argument `attribute` for inspecting attributes in the entities that conform the stored results
 		Map<String, Binding> map2 = new HashMap<String, Binding>();
 		map2.put("product_id", new Binding("review", "Review", "product"));
 		
-		e1.executeSelect("result",new String[] {}, "select `Product.@id` as p_id, Product.* from Product where `Product.@id` = \"${product_id}\"", map2);
+		e1.executeSelect("result", "select `Product.@id` as p_id, Product.* from Product where `Product.@id` = \"${product_id}\"", map2);
 		
 		//List<Entity> result = buildResult("result", );
 		
@@ -145,8 +144,8 @@ public class TestSelect {
 		System.out.println("Final Result:");
 		
 		Map<String, TyphonType> attributes = new HashMap<>();
-		attributes.put("description", new StringType());
-		attributes.put("name", new StringType());
+		attributes.put("description",TyphonType.STRING);
+		attributes.put("name", TyphonType.STRING);
 		WorkingSet result = store.computeResult("result", new String[] { "product" }, new EntityModel("Product", attributes));
 		
 		for (Entity e : result.get("product")) {
---
+++ typhonql/src/nl/cwi/swat/typhonql/backend/test/TestSelect2.java
@@ -0,0 +1,60 @@
+package nl.cwi.swat.typhonql.backend.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import nl.cwi.swat.typhonql.backend.Binding;
+import nl.cwi.swat.typhonql.backend.Engine;
+import nl.cwi.swat.typhonql.backend.EntityModel;
+import nl.cwi.swat.typhonql.backend.MariaDBEngine;
+import nl.cwi.swat.typhonql.backend.MongoDBEngine;
+import nl.cwi.swat.typhonql.backend.ResultStore;
+import nl.cwi.swat.typhonql.backend.TyphonType;
+import nl.cwi.swat.typhonql.workingset.Entity;
+import nl.cwi.swat.typhonql.workingset.WorkingSet;
+
+public class TestSelect2 {
+
+	public static void main(String[] args) {
+		
+		ResultStore store = new ResultStore();
+		
+		Engine e1 = new MariaDBEngine(store, "localhost", 3306, "Inventory", "root", "example");
+		Engine e2 = new MongoDBEngine(store, "localhost", 27018, "Reviews", "admin", "admin");
+		
+		e1.executeSelect("user", "select * from User where `User.name` = \"Pablo\"");
+		Map<String, Binding> map1 = new HashMap<String, Binding>();
+		map1.put("user_id", new Binding("user", "User"));
+		e2.executeSelect("review", "Review\n{ user: \"${user_id}\" }", map1);
+		
+		// Binding needs an extra argument `attribute` for inspecting attributes in the entities that conform the stored results
+		Map<String, Binding> map2 = new HashMap<String, Binding>();
+		map2.put("product_id", new Binding("review", "Review", "product"));
+		
+		e1.executeSelect("result", "select `Product.@id` as p_id, Product.* from Product where `Product.@id` = \"${product_id}\"", map2);
+		
+		//List<Entity> result = buildResult("result", );
+		
+		/*
+		for (Entity e: store.getEntities("user")) {
+			System.out.println(e);
+		}
+		
+		for (Entity e: store.getEntities("review")) {
+			System.out.println(e);
+		}*/
+		
+		System.out.println("Final Result:");
+		
+		Map<String, TyphonType> attributes = new HashMap<>();
+		attributes.put("description", TyphonType.STRING);
+		attributes.put("name", TyphonType.STRING);
+		WorkingSet result = store.computeResult("result", new String[] { "product" }, new EntityModel("Product", attributes));
+		
+		for (Entity e : result.get("product")) {
+			System.out.println(e);
+		}
+
+		
+	}
+}
