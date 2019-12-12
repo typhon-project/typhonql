53aeb113e35e646a3cace63d4d365df099fe828b
Pablo Inostroza Valdera <pinoval@gmail.com>
2019-12-12 22:29:36 +0100

    Added setters to entity model

--- typhonql/src/nl/cwi/swat/typhonql/backend/EntityModel.java
+++ typhonql/src/nl/cwi/swat/typhonql/backend/EntityModel.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class EntityModel {
+
 	private String entityName;
 	private Map<String, TyphonType> attributes;
 	private Map<String, EntityModel> entities;
@@ -38,4 +39,17 @@ public class EntityModel {
 		return entities;
 	}
 
+	public void setEntityName(String entityName) {
+		this.entityName = entityName;
+	}
+
+	public void setAttributes(Map<String, TyphonType> attributes) {
+		this.attributes = attributes;
+	}
+
+	public void setEntities(Map<String, EntityModel> entities) {
+		this.entities = entities;
+	}
+
+	
 }
