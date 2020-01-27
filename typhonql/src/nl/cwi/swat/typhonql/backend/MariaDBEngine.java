d35fc40a71403b127e696c94190118c0993f0555
Pablo Inostroza Valdera <pinoval@gmail.com>
2020-01-27 14:35:47 +0100

    Fix

--- typhonql/src/nl/cwi/swat/typhonql/backend/MariaDBEngine.java
+++ typhonql/src/nl/cwi/swat/typhonql/backend/MariaDBEngine.java
@@ -1,9 +1,51 @@
 package nl.cwi.swat.typhonql.backend;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Map;
+
 public class MariaDBEngine extends SQLEngine {
+
+	private String host;
+	private int port;
+	private String dbName;
+	private String user;
+	private String password;
 
 	public MariaDBEngine(ResultStore store, String host, int port, String dbName, String user, String password) {
 		super(store, host, port, dbName, user, password);
+		initializeDriver();
+	}
+
+	@Override
+	protected ResultIterator performSelect(String query) {
+		try {
+			Connection connection = DriverManager
+					.getConnection(getConnectionString(host, port, dbName, user, password));
+			Statement stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			return new SQLResultIterator(rs);
+
+		} catch (SQLException e1) {
+			throw new RuntimeException(e1);
+		}
+	}
+	
+	@Override
+	protected ResultIterator performSelect(String query, Map<String, String> bindings) {
+		try {
+			Connection connection = DriverManager
+					.getConnection(getConnectionString(host, port, dbName, user, password));
+			Statement stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			return new SQLResultIterator(rs);
+
+		} catch (SQLException e1) {
+			throw new RuntimeException(e1);
+		}
 	}
 
 	@Override
