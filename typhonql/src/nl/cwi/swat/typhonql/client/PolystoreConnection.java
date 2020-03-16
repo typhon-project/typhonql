03d6bb1d88e8a2115ff6b7acbc7717ba9c3a12d9
Pablo Inostroza Valdera <pinoval@gmail.com>
2020-03-16 12:05:48 +0100

    Addded CommandResult.fromIValue method

--- typhonql/src/nl/cwi/swat/typhonql/client/PolystoreConnection.java
+++ typhonql/src/nl/cwi/swat/typhonql/client/PolystoreConnection.java
@@ -19,43 +19,18 @@ public abstract class PolystoreConnection {
 
 	public CommandResult executeUpdate(String query) {
 		IValue val = evaluateUpdate(query);
-		ITuple tuple = (ITuple) val;
-		IInteger n = (IInteger) tuple.get(0);
-		IMap smap = (IMap) tuple.get(1);
-		Map<String, String> map = new HashMap<String, String>();
-		Iterator<Entry<IValue, IValue>> iter = smap.entryIterator();
-		while (iter.hasNext()) {
-			Entry<IValue, IValue> entry = iter.next();
-			map.put(((IString) entry.getKey()).getValue(), ((IString) entry.getValue()).getValue());
-		}
-		return new CommandResult(((IInteger) n).intValue(), map);
-
+		return CommandResult.fromIValue(val);
 	}
 
 	public CommandResult[] executePreparedUpdate(String preparedStatement, String[] columnNames, String[][] values) {
 		IValue v = evaluatePreparedStatementQuery(preparedStatement, columnNames, values);
-		if (v instanceof IList) {
-			Iterator<IValue> iter0 = ((IList) v).iterator();
-			List<CommandResult> results = new ArrayList<CommandResult>();
-			while (iter0.hasNext()) {
-				IValue val = iter0.next();
-				if (val instanceof ITuple) {
-					ITuple tuple = (ITuple) val;
-					IInteger n = (IInteger) tuple.get(0);
-					IMap smap = (IMap) tuple.get(1);
-					Map<String, String> map = new HashMap<String, String>();
-					Iterator<Entry<IValue, IValue>> iter = smap.entryIterator();
-					while (iter.hasNext()) {
-						Entry<IValue, IValue> entry = iter.next();
-						map.put(((IString) entry.getKey()).getValue(), ((IString) entry.getValue()).getValue());
-					}
-					results.add(new CommandResult(((IInteger) n).intValue(), map));
-				} else
-					throw new RuntimeException("Each result row must be a tuple");
-			}
-			return results.toArray(new CommandResult[0]);
-		} else
-			throw new RuntimeException("The result must be a list");
+		Iterator<IValue> iter0 = ((IList) v).iterator();
+		List<CommandResult> results = new ArrayList<CommandResult>();
+		while (iter0.hasNext()) {
+			IValue val = iter0.next();
+			results.add(CommandResult.fromIValue(val));		
+		}
+		return results.toArray(new CommandResult[0]);
 	}
 
 	protected abstract IValue evaluatePreparedStatementQuery(String preparedStatement, String[] columnNames,
