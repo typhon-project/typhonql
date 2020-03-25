b4a434ce8daa58a76db02db2a73c5a323e95334d
pinoval <pinoval@gmail.com>
2020-03-25 14:47:02 +0100

    Adapted test

--- typhonql/src/nl/cwi/swat/typhonql/backend/test/TestSelect2.java
+++ typhonql/src/nl/cwi/swat/typhonql/backend/test/TestSelect2.java
@@ -1,6 +1,8 @@
 package nl.cwi.swat.typhonql.backend.test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -8,54 +10,44 @@ import java.util.Map;
 import java.util.function.Consumer;
 
 import nl.cwi.swat.typhonql.backend.Binding;
-import nl.cwi.swat.typhonql.backend.EntityModel;
 import nl.cwi.swat.typhonql.backend.Field;
 import nl.cwi.swat.typhonql.backend.MariaDBEngine;
 import nl.cwi.swat.typhonql.backend.MongoDBEngine;
 import nl.cwi.swat.typhonql.backend.Record;
 import nl.cwi.swat.typhonql.backend.ResultStore;
-import nl.cwi.swat.typhonql.backend.TyphonType;
-import nl.cwi.swat.typhonql.workingset.Entity;
-import nl.cwi.swat.typhonql.workingset.WorkingSet;
+import nl.cwi.swat.typhonql.backend.rascal.Path;
+import nl.cwi.swat.typhonql.client.resulttable.ResultTable;
 
 public class TestSelect2 {
 
 	public static void main(String[] args) {
-		/*
 		ResultStore store = new ResultStore();
 		
 		Map<String, String> uuids = new HashMap<String, String>();
 		List<Consumer<List<Record>>> script = new ArrayList<>();
-
 		
 		MariaDBEngine e1 = new MariaDBEngine(store, script, uuids, "localhost", 3306, "Inventory", "root", "example");
 		MongoDBEngine e2 = new MongoDBEngine(store, script, uuids, "localhost", 27018, "Reviews", "admin", "admin");
 		
-		e1.executeSelect("user", "select u.`User.@id` as `u.User.@id` from User u where u.`User.name` = \"Claudio\"");
+		e2.executeFindWithProjection("Reviews","Review","{\"contents\": \"***\"}","{\"_id\": 1, \"user\": 1}",
+				Collections.EMPTY_MAP, 
+				Arrays.asList(new Path("Reviews", "r", "Review", new String[] { "user" })));
+		
 		LinkedHashMap<String, Binding> map1 = new LinkedHashMap<String, Binding>();
-		map1.put("user_id", new Field("user", "u", "User"));
-		e2.executeFind("review", "Review", "{ user: ${user_id} }", map1);
+		map1.put("r_1", new Field("Reviews", "r", "Review", "@id"));
 		
-		// Binding needs an extra argument `attribute` for inspecting attributes in the entities that conform the stored results
-		LinkedHashMap<String, Binding> map2 = new LinkedHashMap<String, Binding>();
-		map2.put("product_id", new Field("review", "dummy", "Review", "product"));
+		e1.executeSelect("Inventory", 
+				"select `u`.`User.name` as `u.User.name`, `u`.`User.@id` as `u.User.@id` \nfrom `User` as `u` left outer join `Review.user-User.reviews` as `junction_reviews$0` on (`junction_reviews$0`.`User.reviews`) = (`u`.`User.@id`)\nwhere (`junction_reviews$0`.`Review.user`) = (${r_1});",
+				map1, Arrays.asList(new Path("Inventory", "u", "User", new String[] { "name" })));
 		
-		e1.executeSelect("result", 
-				"select p.`Product.@id` as `p.Product.@id`, p.`Product.name` as `p.Product.name`, p.`Product.description` as `p.Product.description` from Product p where p.`Product.@id` = ${product_id}", map2);
 		
-
-
 		System.out.println("Final Result:");
 		
-		Map<String, TyphonType> attributes = new HashMap<>();
-		attributes.put("description", TyphonType.STRING);
-		attributes.put("name", TyphonType.STRING);
-		WorkingSet result = store.computeResult("result", new String[] { "p" }, new EntityModel("Product", attributes));
+		ResultTable result = store.computeResultTable(script,
+				Arrays.asList(
+						new Path("Inventory", "u", "User", new String[] { "name" }),
+						new Path("Reviews", "r", "Review", new String[] { "user" }) ));
 		
-		for (Entity e : result.get("p")) {
-			System.out.println(e);
-		}
-
-		*/
+		result.print();
 	}
 }
