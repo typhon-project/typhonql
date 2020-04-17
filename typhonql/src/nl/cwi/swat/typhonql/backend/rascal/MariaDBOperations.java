8469c22e3e5963a3628863bd99b4f8cef5f3cb62
Pablo Inostroza Valdera <pinoval@gmail.com>
2020-04-17 17:43:09 +0200

    Adapting for global statements

--- typhonql/src/nl/cwi/swat/typhonql/backend/rascal/MariaDBOperations.java
+++ typhonql/src/nl/cwi/swat/typhonql/backend/rascal/MariaDBOperations.java
@@ -27,98 +27,123 @@ import nl.cwi.swat.typhonql.backend.Record;
 import nl.cwi.swat.typhonql.backend.ResultStore;
 
 public class MariaDBOperations implements Operations {
-	
+
 	Map<String, Connection> connections;
-	
+
 	public MariaDBOperations(Map<String, ConnectionData> connections) throws SQLException {
 		initializeDriver();
+		boolean first = true;
 		for (Entry<String, ConnectionData> entry : connections.entrySet()) {
 			String dbName = entry.getKey();
 			ConnectionData data = entry.getValue();
-			Connection connection = DriverManager
-					.getConnection(getConnectionString(data.getHost(), data.getPort(), dbName, data.getUser(), data.getPassword()));
+			
+			if (first) {
+				Connection globalConnection = DriverManager.getConnection(
+						getConnectionString(data.getHost(), data.getPort(), "", data.getUser(), data.getPassword()));
+				this.connections.put("", globalConnection);
+			}
+			
+			Connection connection = DriverManager.getConnection(
+					getConnectionString(data.getHost(), data.getPort(), dbName, data.getUser(), data.getPassword()));
 			this.connections.put(dbName, connection);
 		}
 	}
-	
+
 	private String getConnectionString(String host, int port, String dbName, String user, String password) {
 		return "jdbc:mariadb://" + host + ":" + port + "/" + dbName + "?user=" + user + "&password=" + password;
 	}
-	
 
-	private ICallableValue makeExecuteQuery(ResultStore store, List<Consumer<List<Record>>> script, TyphonSessionState state, Map<String, String> uuids, FunctionType executeType, IEvaluatorContext ctx, IValueFactory vf, TypeFactory tf) {
+	private ICallableValue makeExecuteQuery(ResultStore store, List<Consumer<List<Record>>> script,
+			TyphonSessionState state, Map<String, String> uuids, FunctionType executeType, IEvaluatorContext ctx,
+			IValueFactory vf, TypeFactory tf) {
 		return makeFunction(ctx, state, executeType, args -> {
 			String resultId = ((IString) args[0]).getValue();
 			String dbName = ((IString) args[1]).getValue();
 			String query = ((IString) args[2]).getValue();
 			IMap bindings = (IMap) args[3];
 			IList signatureList = (IList) args[4];
-			
+
 			Map<String, Binding> bindingsMap = rascalToJavaBindings(bindings);
 			List<Path> signature = rascalToJavaSignature(signatureList);
-			
+
 			Connection connection = connections.get(dbName);
 			new MariaDBEngine(store, script, uuids, connection).executeSelect(resultId, query, bindingsMap, signature);
-			
-			//sessionData.put(resultName, query);
-			return ResultFactory.makeResult(tf.voidType(), null, ctx);
-		});
-	}
-	
-	private ICallableValue makeExecuteStatement(ResultStore store, List<Consumer<List<Record>>> script, TyphonSessionState state, Map<String, String> uuids, FunctionType executeStmtType, IEvaluatorContext ctx, IValueFactory vf, TypeFactory tf) {
-		return makeFunction(ctx, state, executeStmtType, args -> {
-			String dbName = ((IString) args[0]).getValue();
-			String query = ((IString) args[1]).getValue();
-			IMap bindings = (IMap) args[2];
-			
-			Map<String, Binding> bindingsMap = rascalToJavaBindings(bindings);
-			
-			Connection connection = connections.get(dbName);
-			new MariaDBEngine(store, script, uuids, connection).executeUpdate(query, bindingsMap);
-			
-			//sessionData.put(resultName, query);
-			return ResultFactory.makeResult(tf.voidType(), null, ctx);
-		});
-	}
-	
-	private ICallableValue makeExecuteGlobalStatement(ResultStore store, List<Consumer<List<Record>>> script, TyphonSessionState state, Map<String, String> uuids, FunctionType executeStmtType, IEvaluatorContext ctx, IValueFactory vf, TypeFactory tf) {
-		return makeFunction(ctx, state, executeStmtType, args -> {
-			String dbName = ((IString) args[0]).getValue();
-			String query = ((IString) args[1]).getValue();
-			IMap bindings = (IMap) args[2];
-			
-			Map<String, Binding> bindingsMap = rascalToJavaBindings(bindings);
-			
-			Connection connection = connections.get(dbName);
-			
-			new MariaDBEngine(store, script, uuids, connection).executeUpdate(query, bindingsMap);
-			
-			//sessionData.put(resultName, query);
+
+			// sessionData.put(resultName, query);
 			return ResultFactory.makeResult(tf.voidType(), null, ctx);
 		});
 	}
 
-	public ITuple newSQLOperations(ResultStore store, List<Consumer<List<Record>>> script, TyphonSessionState state, Map<String, String> uuids, IEvaluatorContext ctx, IValueFactory vf, TypeFactory tf) {
+	private ICallableValue makeExecuteStatement(ResultStore store, List<Consumer<List<Record>>> script,
+			TyphonSessionState state, Map<String, String> uuids, FunctionType executeStmtType, IEvaluatorContext ctx,
+			IValueFactory vf, TypeFactory tf) {
+		return makeFunction(ctx, state, executeStmtType, args -> {
+			String dbName = ((IString) args[0]).getValue();
+			String query = ((IString) args[1]).getValue();
+			IMap bindings = (IMap) args[2];
+
+			Map<String, Binding> bindingsMap = rascalToJavaBindings(bindings);
+
+			Connection connection = connections.get(dbName);
+			new MariaDBEngine(store, script, uuids, connection).executeUpdate(query, bindingsMap);
+
+			// sessionData.put(resultName, query);
+			return ResultFactory.makeResult(tf.voidType(), null, ctx);
+		});
+	}
+
+	private ICallableValue makeExecuteGlobalStatement(ResultStore store, List<Consumer<List<Record>>> script,
+			TyphonSessionState state, Map<String, String> uuids, FunctionType executeStmtType, IEvaluatorContext ctx,
+			IValueFactory vf, TypeFactory tf) {
+		return makeFunction(ctx, state, executeStmtType, args -> {
+			String dbName = ((IString) args[0]).getValue();
+			String query = ((IString) args[1]).getValue();
+			IMap bindings = (IMap) args[2];
+
+			Map<String, Binding> bindingsMap = rascalToJavaBindings(bindings);
+
+			Connection connection = connections.get("");
+
+			new MariaDBEngine(store, script, uuids, connection).executeUpdate(query, bindingsMap);
+
+			// sessionData.put(resultName, query);
+			return ResultFactory.makeResult(tf.voidType(), null, ctx);
+		});
+	}
+
+	public ITuple newSQLOperations(ResultStore store, List<Consumer<List<Record>>> script, TyphonSessionState state,
+			Map<String, String> uuids, IEvaluatorContext ctx, IValueFactory vf, TypeFactory tf) {
 		Type aliasedTuple = Objects.requireNonNull(ctx.getCurrentEnvt().lookupAlias("SQLOperations"));
 		while (aliasedTuple.isAliased()) {
 			aliasedTuple = aliasedTuple.getAliased();
 		}
 
-		FunctionType executeQueryType = (FunctionType)aliasedTuple.getFieldType("executeQuery");
-		FunctionType executeStatementType = (FunctionType)aliasedTuple.getFieldType("executeStatement");
-				
-		return vf.tuple(
-            makeExecuteQuery(store, script, state, uuids, executeQueryType, ctx, vf, tf),
-            makeExecuteStatement(store, script, state, uuids, executeStatementType, ctx, vf, tf),
-            makeExecuteGlobalStatement(store, script, state, uuids, executeStatementType, ctx, vf, tf)
-		);
+		FunctionType executeQueryType = (FunctionType) aliasedTuple.getFieldType("executeQuery");
+		FunctionType executeStatementType = (FunctionType) aliasedTuple.getFieldType("executeStatement");
+		FunctionType executeGlobalStatementType = (FunctionType) aliasedTuple.getFieldType("executeGlobalStatement");
+
+		return vf.tuple(makeExecuteQuery(store, script, state, uuids, executeQueryType, ctx, vf, tf),
+				makeExecuteStatement(store, script, state, uuids, executeStatementType, ctx, vf, tf),
+				makeExecuteGlobalStatement(store, script, state, uuids, executeGlobalStatementType, ctx, vf, tf));
 	}
-	
+
 	private void initializeDriver() {
 		try {
 			Class.forName("org.mariadb.jdbc.Driver");
 		} catch (ClassNotFoundException e) {
 			throw new RuntimeException("MariaDB driver not found", e);
 		}
+	}
+
+	public void close() {
+		for (Entry<String, Connection> conn : connections.entrySet()) {
+			try {
+				conn.getValue().close();
+			} catch (SQLException e) {
+				throw new RuntimeException("Problems closing connection " + conn.getKey(), e);
+			}
+
+		}
+
 	}
 }
