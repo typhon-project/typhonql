760ab8ce3976e74d4fe1fdd1c09be5e150e639d9
Pablo Inostroza Valdera <pinoval@gmail.com>
2020-04-21 17:35:15 +0200

    Added parent class for DAL resources

--- typhonql-server/src/main/java/engineering/swat/typhonql/server/crud/EntitiesResource.java
+++ typhonql-server/src/main/java/engineering/swat/typhonql/server/crud/EntitiesResource.java
@@ -1,57 +1,50 @@
 package engineering.swat.typhonql.server.crud;
 
-import java.io.IOException;
+import java.net.URI;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
-import javax.servlet.ServletContext;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import engineering.swat.typhonql.server.QLRestServer;
-import engineering.swat.typhonql.server.QueryEngine;
 import nl.cwi.swat.typhonql.client.CommandResult;
 import nl.cwi.swat.typhonql.client.resulttable.ResultTable;
 import nl.cwi.swat.typhonql.workingset.Entity;
 
 @Path("/{entityName}")
-public class EntitiesResource {
+public class EntitiesResource extends TyphonDALResource {
 
 	private static final Logger logger = LogManager.getLogger(EntitiesResource.class);
-	
-	@Context
-	ServletContext context;
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<Entity> getEntites(@PathParam("entityName") String entityName) {
-		try {
-			logger.trace("Getting all entities of type: " + entityName);
-			ResultTable result = getEngine().executeQuery("from " + entityName + " e select e");
-			return result.getValues().stream().map(vs -> new Entity(entityName, (String) vs.get(0)))
-					.collect(Collectors.toList());
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-
-	}
-
-	private QueryEngine getEngine() {
-		return (QueryEngine) context.getAttribute(QLRestServer.QUERY_ENGINE);
+		logger.trace("Getting all entities of type: " + entityName);
+		ResultTable result = getEngine().executeQuery(getModel(), getDatabaseInfo(),
+				"from " + entityName + " e select e");
+		return result.getValues().stream().map(vs -> new Entity(entityName, (String) vs.get(0)))
+				.collect(Collectors.toList());
 	}
 
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
-	public CommandResult createEntity(Entity entity) {
-		return new CommandResult(0);
+	public Response createEntity(@PathParam("entityName") String entityName, Map<String, String> fields) {
+		String query = "insert " + entityName + " { " + serializeFields(fields) + "}";
+ 		try {
+			CommandResult cr = getEngine().executeUpdate(getModel(), getDatabaseInfo(), query);
+			return Response.created(URI.create("/" + entityName + "/" + cr.getCreatedUuids().values().iterator().next())).build();
+		} catch (RuntimeException e) {
+			return Response.serverError().build();
+		}
 	}
 
 }
---
+++ typhonql-server/src/main/java/engineering/swat/typhonql/server/crud/TyphonDALResource.java
@@ -0,0 +1,43 @@
+package engineering.swat.typhonql.server.crud;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.servlet.ServletContext;
+import javax.ws.rs.core.Context;
+
+import engineering.swat.typhonql.server.QLRestServer;
+import nl.cwi.swat.typhonql.client.DatabaseInfo;
+import nl.cwi.swat.typhonql.client.XMIPolystoreConnection;
+
+public abstract class TyphonDALResource {
+
+	@Context
+	private ServletContext context;
+
+	protected XMIPolystoreConnection getEngine() {
+		return (XMIPolystoreConnection) context.getAttribute(QLRestServer.QUERY_ENGINE);
+	}
+
+	protected String getModel() {
+		return (String) context.getAttribute(QLRestServer.MODEL_FOR_DAL);
+	}
+
+	protected List<DatabaseInfo> getDatabaseInfo() {
+		return (List<DatabaseInfo>) context.getAttribute(QLRestServer.DATABASE_INFO_FOR_DAL);
+	}
+
+	protected String serializeFields(Map<String, String> fields) {
+		return String.join(", ",
+				fields.entrySet().stream().map(e -> "\"" + e.getKey() + "\" : "
+						+ serializeValue(e.getValue())).collect(Collectors.toList()).toArray(new String[0]));
+	}
+
+	private String serializeValue(Object v) {
+		if (v instanceof String)
+			return "\"" + v + "\"";
+		else
+			return v.toString();
+	}
+}
