0a8302f74b5c24f7c313a9910fa6751dccf76f5a
Pablo Inostroza Valdera <pinoval@gmail.com>
2020-04-24 14:19:28 +0200

    Edited polystore parameters

--- typhonql/src/nl/cwi/swat/typhonql/workingset/JsonSerializableResult.java
+++
@@ -1,8 +0,0 @@
-package nl.cwi.swat.typhonql.workingset;
-
-import java.io.IOException;
-import java.io.OutputStream;
-
-public interface JsonSerializableResult {
-	void serializeJSON(OutputStream target) throws IOException;
-}
