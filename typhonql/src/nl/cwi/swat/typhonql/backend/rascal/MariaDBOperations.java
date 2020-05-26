1fc9e8eceb2ab4507410a56293bfcbb5a81bfd4f
Pablo Inostroza Valdera <pinoval@gmail.com>
2020-05-26 15:51:03 +0200

    Fixed buf by making sql connections lazy

--- typhonql/src/nl/cwi/swat/typhonql/backend/rascal/MariaDBOperations.java
+++ typhonql/src/nl/cwi/swat/typhonql/backend/rascal/MariaDBOperations.java
@@ -117,7 +117,7 @@ public class MariaDBOperations implements Operations, AutoCloseable {
 		FunctionType executeStatementType = (FunctionType) aliasedTuple.getFieldType("executeStatement");
 		FunctionType executeGlobalStatementType = (FunctionType) aliasedTuple.getFieldType("executeGlobalStatement");
 		
-		BiFunction<String, Boolean, MariaDBEngine> getEngine = (dbName, scoped) -> new MariaDBEngine(store, script, updates, uuids, getConnection(dbName, scoped));
+		BiFunction<String, Boolean, MariaDBEngine> getEngine = (dbName, scoped) -> new MariaDBEngine(store, script, updates, uuids, () -> getConnection(dbName, scoped));
 
 		return vf.tuple(makeExecuteQuery(getEngine, state, executeQueryType, ctx, vf),
 				makeExecuteStatement(getEngine, state, executeStatementType, ctx, vf),
