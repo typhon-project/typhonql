973649365885552de003493c045609663888577f
Pablo Inostroza Valdera <pinoval@gmail.com>
2020-06-15 16:46:06 +0200

    Added systematic mapping of properties to the right type

--- typhonql/src/nl/cwi/swat/typhonql/backend/neo4j/Neo4JIterator.java
+++ typhonql/src/nl/cwi/swat/typhonql/backend/neo4j/Neo4JIterator.java
@@ -4,6 +4,9 @@ import java.util.Iterator;
 import java.util.List;
 
 import org.neo4j.driver.Record;
+import org.neo4j.driver.Value;
+import org.neo4j.driver.internal.types.InternalTypeSystem;
+import org.neo4j.driver.types.TypeSystem;
 
 import nl.cwi.swat.typhonql.backend.ResultIterator;
 
@@ -12,11 +15,11 @@ public class Neo4JIterator implements ResultIterator {
 	private List<Record> records;
 	private Iterator<Record> iterator;
 	private Record current;
+	private static TypeSystem TYPES = InternalTypeSystem.TYPE_SYSTEM;
 	
 	public Neo4JIterator(List<Record> records) {
 		this.records = records;
 		this.iterator = records.iterator();
-		
 	}
 
 	@Override
@@ -32,13 +35,27 @@ public class Neo4JIterator implements ResultIterator {
 
 	@Override
 	public String getCurrentId(String label, String type) {
-		return current.get("@id", "");
+		return current.get(label + "." + type + ".@id", "");
 	}
-
+	
 	@Override
 	public Object getCurrentField(String label, String type, String name) {
-		// TODO Auto-generated method stub
-		return null;
+		Value v = current.get(label + "." + type + "." + name);
+		return getFieldForType(v);
+	}
+
+	private Object getFieldForType(Value v) {
+		if (v.hasType(TYPES.STRING())) {
+			return v.asString();
+		}
+		else if (v.hasType(TYPES.BOOLEAN())) {
+			return v.asBoolean();
+		}
+		else if (v.hasType(TYPES.INTEGER())) {
+			return v.asInt();
+		}
+		else
+			throw new RuntimeException("There is no mapper for Neo4J type " + v.type());
 	}
 
 	@Override
