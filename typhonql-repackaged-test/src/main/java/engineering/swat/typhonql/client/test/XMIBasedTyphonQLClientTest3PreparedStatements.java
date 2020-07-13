08ebf5c285081f1ab2fd4da412420dc7eb56f91e
Davy Landman <davy.landman@swat.engineering>
2020-07-13 10:18:30 +0200

    Reporposed the repackaged test for simple debugging of xmi poly store

--- typhonql-repackaged-test/src/main/java/engineering/swat/typhonql/client/test/XMIBasedTyphonQLClientTest3PreparedStatements.java
+++ typhonql-repackaged-test/src/main/java/engineering/swat/typhonql/client/test/XMIBasedTyphonQLClientTest3PreparedStatements.java
@@ -6,23 +6,19 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Collections;
 
-import nl.cwi.swat.typhonql.DBType;
-import nl.cwi.swat.typhonql.MariaDB;
-import nl.cwi.swat.typhonql.MongoDB;
 import nl.cwi.swat.typhonql.client.CommandResult;
 import nl.cwi.swat.typhonql.client.DatabaseInfo;
-import nl.cwi.swat.typhonql.client.PolystoreConnection;
 import nl.cwi.swat.typhonql.client.XMIPolystoreConnection;
 import nl.cwi.swat.typhonql.client.resulttable.ResultTable;
-import nl.cwi.swat.typhonql.workingset.json.WorkingSetJSON;
 
 public class XMIBasedTyphonQLClientTest3PreparedStatements {
 	public static void main(String[] args) throws IOException, URISyntaxException {
 		DatabaseInfo[] infos = new DatabaseInfo[] {
-				new DatabaseInfo("localhost", 27017, "Reviews", DBType.documentdb, new MongoDB().getName(),
+				new DatabaseInfo("localhost", 27017, "Reviews", "mongodb", "mongodb",
 						"admin", "admin"),
-				new DatabaseInfo("localhost", 3306, "Inventory", DBType.relationaldb, new MariaDB().getName(),
+				new DatabaseInfo("localhost", 3306, "Inventory", "mariadb", "mariadb",
 						"root", "example") };
 		/*
 		if (args == null || args.length != 1 && args[0] == null) {
@@ -35,9 +31,9 @@ public class XMIBasedTyphonQLClientTest3PreparedStatements {
 		
 		String xmiString = String.join("\n", Files.readAllLines(Paths.get(new URI(fileName))));
 
-		PolystoreConnection conn = new XMIPolystoreConnection(xmiString, Arrays.asList(infos));
+		XMIPolystoreConnection conn = new XMIPolystoreConnection();
 		
-		CommandResult[] crs = conn.executePreparedUpdate("insert \n" + 
+		CommandResult[] crs = conn.executePreparedUpdate(xmiString, Arrays.asList(infos), Collections.emptyMap(), "insert \n" + 
 				"	Product {name: ??name, description: ??description }", 
 				new String[]{"description", "name"}, 
 				new String[][]{ { "\"Best beer\"", "\"Omer\""}, { "\"Good IPA\"", "\"Jopen IPA\""}});
@@ -49,13 +45,13 @@ public class XMIBasedTyphonQLClientTest3PreparedStatements {
 		}
 		System.out.println("COMMAND RESULTS");
 		for (CommandResult cr : crs)
-			WorkingSetJSON.toJSON(cr, System.out);
+			System.out.println(cr);
 		System.out.println("END COMMAND RESULTS");
 		
 		
-		ResultTable iv = conn.executeQuery("from Product p select p.name");
+		ResultTable iv = conn.executeQuery(xmiString, Arrays.asList(infos),"from Product p select p.name");
 		System.out.println("RESULT TABLE");
-		iv.print();
+		System.out.println(iv.toString());
 		
 	}
 }
--- typhonql-repackaged-test/src/main/java/engineering/swat/typhonql/client/test/XMIBasedTyphonQLSimpleQuery.java
+++ typhonql-repackaged-test/src/main/java/engineering/swat/typhonql/client/test/XMIBasedTyphonQLSimpleQuery.java
@@ -2,10 +2,11 @@ package engineering.swat.typhonql.client.test;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Collections;
 import java.util.List;
 
+import nl.cwi.swat.typhonql.client.CommandResult;
 import nl.cwi.swat.typhonql.client.DatabaseInfo;
-import nl.cwi.swat.typhonql.client.PolystoreConnection;
 import nl.cwi.swat.typhonql.client.XMIPolystoreConnection;
 import nl.cwi.swat.typhonql.client.resulttable.ResultTable;
 
@@ -23,9 +24,17 @@ public class XMIBasedTyphonQLSimpleQuery {
 				USER, PASSWORD);
 		
 		String xmiString = PolystoreAPIHelper.readHttpModel(HOST, PORT, USER, PASSWORD);
+		//System.err.println(xmiString);
+		//System.err.println(infos);
 
-		PolystoreConnection conn = new XMIPolystoreConnection(xmiString, infos);
+		XMIPolystoreConnection conn = new XMIPolystoreConnection();
 		
-		ResultTable rt = conn.executeQuery("from Product p select p");
+		//ResultTable rt = conn.executeQuery(xmiString, infos, "from Product p select p.name");
+		//ResultTable rt = conn.executeQuery(xmiString, infos, "from Product p, Review r select r.content where p.reviews == r, p.@id == #tv");
+		//CommandResult rt = conn.executeUpdate(xmiString, infos, Collections.emptyMap(), "update User u where u.@id == #davy set {photoURL: \"other\", name: \"Landman\"}");
+		ResultTable rt = conn.executeQuery(xmiString, infos, "from User u select u.photoURL, u.name");
+
+		System.out.println(rt);
+
 	}
 }
