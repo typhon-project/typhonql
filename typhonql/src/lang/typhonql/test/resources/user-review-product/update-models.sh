86d718e9212e6000a78a0e1158b023ea7e1f3b18
Davy Landman <davy.landman@swat.engineering>
2020-07-22 15:50:52 +0200

    Added @MarieSaphira script to update the insertModels.js file

---
+++ typhonql/src/lang/typhonql/test/resources/user-review-product/update-models.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e 
+dlmodel=$(<"$1")
+mlmodel=$(<"$2")
+dlmodelRE1=${dlmodel//\"/\\\"}
+dlmodelRE=${dlmodelRE1//[$'\t\r\n ']}
+mlmodelRE1=${mlmodel//\"/\\\"}
+mlmodelRE=${mlmodelRE1//[$'\t\r\n ']}
+echo "db.models.insert([{\"_id\":UUID(), \"version\":1, \"initializedDatabases\": false, \"initializedConnections\":true, \"contents\":\"$dlmodelRE\", \"type\":\"DL\", \"dateReceived\":ISODate(), \"_class\":\"com.clms.typhonapi.models.Model\" }, {\"_id\":UUID(), \"version\":1, \"initializedDatabases\":false, \"initializedConnections\":false, \"contents\":\"$mlmodelRE\", \"type\":\"ML\", \"dateReceived\":ISODate(), \"_class\":\"com.clms.typhonapi.models.Model\" }]);" > models/insertModels.js
