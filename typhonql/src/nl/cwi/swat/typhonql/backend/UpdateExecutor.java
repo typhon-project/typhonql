86c7c8f4fe5b3c76ad020f2bdb49fe6673cb7f7a
Pablo Inostroza Valdera <pinoval@gmail.com>
2020-08-14 17:28:49 +0200

    Updates base class consider multiple bindings and multiple uuids

--- typhonql/src/nl/cwi/swat/typhonql/backend/UpdateExecutor.java
+++ typhonql/src/nl/cwi/swat/typhonql/backend/UpdateExecutor.java
@@ -16,67 +16,119 @@
 
 package nl.cwi.swat.typhonql.backend;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.Optional;
 import java.util.UUID;
 
-import org.rascalmpl.interpreter.utils.RuntimeExceptionFactory;
-import org.rascalmpl.values.ValueFactoryFactory;
-
 public abstract class UpdateExecutor {
-	
+
+	private String query;
 	private ResultStore store;
 	private List<Runnable> updates;
 	private Map<String, Binding> bindings;
-	private Map<String, UUID> uuids;
+	private Map<String, List<UUID>> uuids;
+	private Optional<MultipleBindings> mBindings;
 
-	public UpdateExecutor(ResultStore store, List<Runnable> updates, Map<String, UUID> uuids, Map<String, Binding> bindings) {
+	public UpdateExecutor(String query, ResultStore store, List<Runnable> updates, Map<String, List<UUID>> uuids,
+			Map<String, Binding> bindings) {
+		this(query, store, updates, uuids, bindings, Optional.empty());
+	}
+
+	public UpdateExecutor(String query, ResultStore store, List<Runnable> updates, Map<String, List<UUID>> uuids,
+			Map<String, Binding> bindings, Optional<MultipleBindings> mBindings) {
+		this.query = query;
 		this.store = store;
 		this.updates = updates;
 		this.bindings = bindings;
-		this.uuids = uuids;
+		this.uuids = cloneMap(uuids);
+		this.mBindings = mBindings;
 	}
-	
+
 	public void executeUpdate() {
-		executeUpdate(new HashMap<>());
+		Matcher m = Engine.QL_PARAMS.matcher(query);
+		List<String> allVars = new ArrayList<String>();
+		while (m.find()) {
+			String param = m.group(1);
+			allVars.add(param);
+		}
+		if (mBindings.isPresent()) {
+			MultipleBindings mbs = mBindings.get();
+			for (List<String> row : mbs.getValues()) {
+				Map<String, Object> values = toObjectRepresentation(row, mbs.getVarNames(), mbs.getTypesMap(), allVars);
+				executeUpdateOperation(values);
+			}
+		}
+		else
+			executeUpdateOperation(new HashMap<String, Object>());
 	}
-	
-	private void executeUpdate(Map<String, Object> values) {
-		updates.add(() -> {  executeUpdateOperation(values); });
+
+	private Map<String, List<UUID>> cloneMap(Map<String, List<UUID>> uuids) {
+		 Map<String, List<UUID>>  map = new HashMap<>();
+		 for (Entry<String, List<UUID>> e : uuids.entrySet()) {
+			 map.put(e.getKey(), new ArrayList<UUID>(e.getValue()));
+		 }
+		 return map;
 	}
 
 	protected abstract void performUpdate(Map<String, Object> values);
-	
+
 	private void executeUpdateOperation(Map<String, Object> values) {
-		if (values.size() == bindings.size()) {
-			performUpdate(values); 
-		}
-		else {
+		if (values.size() == (bindings.size() + (mBindings.isPresent()?mBindings.get().getVarNames().size():0))) {
+			performUpdate(values);
+		} else {
 			String var = bindings.keySet().iterator().next();
 			Binding binding = bindings.get(var);
 			if (binding instanceof Field) {
 				Field field = (Field) binding;
-				ResultIterator results =  store.getResults(field.getReference());
+				ResultIterator results = store.getResults(field.getReference());
 				if (results == null) {
-					throw new RuntimeException("Results was null for field " + field.toString() + " and store " + store);
+					throw new RuntimeException(
+							"Results was null for field " + field.toString() + " and store " + store);
 				}
-				
+
 				results.beforeFirst();
-				
+
 				while (results.hasNextResult()) {
 					results.nextResult();
-					Object value = (field.getAttribute().equals("@id"))? results.getCurrentId(field.getLabel(), field.getType()) : results.getCurrentField(field.getLabel(), field.getType(), field.getAttribute());
+					Object value = (field.getAttribute().equals("@id"))
+							? results.getCurrentId(field.getLabel(), field.getType())
+							: results.getCurrentField(field.getLabel(), field.getType(), field.getAttribute());
 					values.put(var, value);
 					executeUpdateOperation(values);
 				}
-			}
-			else {
+			} else {
 				GeneratedIdentifier id = (GeneratedIdentifier) binding;
-				values.put(var, uuids.get(id.getName()));
+				UUID uuid = uuids.get(id.getName()).remove(0);
+				values.put(var, uuid);
 				executeUpdateOperation(values);
 			}
-			
+
 		}
 	}
+
+	private Map<String, Object> toObjectRepresentation(List<String> row, List<String> vars,
+			Map<String, String> types, List<String> varsToKeep) {
+		Map<String, Object> vs = new HashMap<String, Object>();
+		for (int i = 0; i < vars.size(); i++) {
+			if (varsToKeep.contains(vars.get(i)))
+				vs.put(vars.get(i), toObjectRepresentation(types.get(vars.get(i)), row.get(i)));
+		}
+		return vs;
+
+	}
+	
+	private Object toObjectRepresentation(String type, String val) {
+		switch(type) {
+		case "string": return val;
+		case "int": return Integer.parseInt(val);
+		case "uuid": return UUID.fromString(val);
+		default: return val;
+		}
+	}
+
 }
