0d326553894dd416e65d49545082e70475cd66c1
Tijs van der Storm <storm@cwi.nl>
2020-10-14 11:39:58 +0200

    Factored out compareTo.

--- typhonql/src/nl/cwi/swat/typhonql/backend/rascal/HavingOperators.java
+++ typhonql/src/nl/cwi/swat/typhonql/backend/rascal/HavingOperators.java
@@ -13,40 +13,29 @@ public class HavingOperators {
 	public static boolean neq(Object x, Object y) {
 		return !eq(x, y);
 	}
-
-
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public static boolean geq(Object x, Object y) {
-		Comparable c1 = (Comparable)x;
-		Comparable c2 = (Comparable)y;
-		int c = c1.compareTo(c2);
-		return c == 0 || c > 0;
-	}
-
-
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public static boolean leq(Object x, Object y) {
-		Comparable c1 = (Comparable)x;
-		Comparable c2 = (Comparable)y;
-		int c = c1.compareTo(c2);
-		return c == 0 || c < 0;
-	}
-
-
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public static boolean lt(Object x, Object y) {
-		Comparable c1 = (Comparable)x;
-		Comparable c2 = (Comparable)y;
-		int c = c1.compareTo(c2);
-		return c < 0;
-	}
 	
 	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public static boolean gt(Object x, Object y) {
+	private static int myCompare(Object x, Object y) {
 		Comparable c1 = (Comparable)x;
 		Comparable c2 = (Comparable)y;
-		int c = c1.compareTo(c2);
-		return c > 0;
+		return c1.compareTo(c2);
+	}
+
+
+	public static boolean geq(Object x, Object y) {
+		return myCompare(x, y) >= 0;
+	}
+
+	public static boolean leq(Object x, Object y) {
+		return myCompare(x, y) <= 0;
+	}
+
+	public static boolean lt(Object x, Object y) {
+		return myCompare(x, y) < 0;
+	}
+	
+	public static boolean gt(Object x, Object y) {
+		return myCompare(x, y) > 0;
 	}
 	
 	public static boolean and(Object x, Object y) {
