6793714c22b5555cc99f60d848498e300a2f2c44
Davy Landman <davy.landman@swat.engineering>
2021-01-13 14:28:55 +0100

    Protected against empty results from for example NLP

--- typhonql/src/nl/cwi/swat/typhonql/backend/QueryExecutor.java
+++ typhonql/src/nl/cwi/swat/typhonql/backend/QueryExecutor.java
@@ -137,12 +137,14 @@ public abstract class QueryExecutor {
 				List<ResultIterator> lst = new ArrayList<>();
 				Field field = (Field) binding;
 				ResultIterator results =  store.getResults(field.getReference());
-				results.beforeFirst();
-				while (results.hasNextResult()) {
-					results.nextResult();
-					Object value = (field.getAttribute().equals("@id"))? results.getCurrentId(field.getLabel(), field.getType()) : results.getCurrentField(field.getLabel(), field.getType(), field.getAttribute());
-					values.put(var, value);
-					lst.add(executeSelect(values));
+				if (results != null) {
+                    results.beforeFirst();
+                    while (results.hasNextResult()) {
+                        results.nextResult();
+                        Object value = (field.getAttribute().equals("@id"))? results.getCurrentId(field.getLabel(), field.getType()) : results.getCurrentField(field.getLabel(), field.getType(), field.getAttribute());
+                        values.put(var, value);
+                        lst.add(executeSelect(values));
+                    }
 				}
 				return new AggregatedResultIterator(lst);
 			}
